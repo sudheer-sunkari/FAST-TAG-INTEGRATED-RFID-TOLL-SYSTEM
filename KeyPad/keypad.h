b495084d02299ee04c69ebcc66b451cee7e65fb7
sudheer-sunkari <sudheersunkari53@gmail.com>
2026-08-23 21:24:47 +0530

    Create keypad.h

---
+++ KeyPad/keypad.h
@@ -0,0 +1,17 @@
+#define ROW0 16  //p1.16
+
+#define	ROW1 17   //P1.17
+
+#define	ROW2 18   //P1.18
+
+#define	ROW3 19  //P1.19
+
+#define	COL0 20   //P1.20
+
+#define COL1 21  //P1.21
+
+#define	COL2 22  //P1.22
+
+#define	COL3 23   //P1.23
+
+void Init_KPM(void);
