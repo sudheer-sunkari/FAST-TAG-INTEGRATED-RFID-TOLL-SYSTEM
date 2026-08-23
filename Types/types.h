5665731de2ab05ac0c138b27002813ada7096362
sudheer-sunkari <sudheersunkari53@gmail.com>
2026-08-23 21:04:13 +0530

    Create types.h

---
+++ Types/types.h
@@ -0,0 +1,16 @@
+typedef unsigned char u8;       // Defines u8 as an unsigned 8-bit character data type
+
+typedef char s8;                // Defines s8 as a signed 8-bit character data type
+
+typedef unsigned short u16;     // Defines u16 as an unsigned 16-bit integer data type
+
+typedef signed short s16;       // Defines s16 as a signed 16-bit integer data type
+
+typedef unsigned int u32;       // Defines u32 as an unsigned 32-bit integer data type
+
+typedef int s32;                // Defines s32 as a signed 32-bit integer data type
+
+typedef float f32;              // Defines f32 as a 32-bit floating-point data type
+
+typedef double f64;             // Defines f64 as a 64-bit double-precision floating-point data type
+
